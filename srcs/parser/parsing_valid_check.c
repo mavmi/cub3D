b40ad01691f3bb94fdb7dfb800e3e52488264254
Христофорова Полина Андреевна <hristoforova@miit.ru>
2022-03-02 17:23:23 +0300

    optimize with error codes

--- srcs/parser/parsing_valid_check.c
+++ srcs/parser/parsing_valid_check.c
@@ -66,14 +66,18 @@ static int	check_map(char **arr, int *i_s, size_t *i_e, t_argums *args)
 
 static int	check_every_str(char **arr, int *i_s, size_t *i_e, t_argums *args)
 {
-	if (arr[*i_s][*i_e] == NO_SIDE || arr[*i_s][*i_e] == SO_SIDE
+	if (arr[*i_s][*i_e] == '\0')
+	{
+		*i_e = 0;
+	}
+	else if (arr[*i_s][*i_e] == NO_SIDE || arr[*i_s][*i_e] == SO_SIDE
 			|| arr[*i_s][*i_e] == WE_SIDE || arr[*i_s][*i_e] == EA_SIDE)
 	{
 		//"take new texture elem" function: give me arr[i_s + i_e]
 		if (pars_valid_txtr((arr[*i_s] + i_e), args->txtr_arr))
 		{
 			printf("1\n");
-			return (error_destroy(args, 'n')); //don't forget free()
+			return (error_destroy(args, 'n'));
 		}
 	}
 	else if (arr[*i_s][*i_e] == FLOOR || arr[*i_s][*i_e] == CEILING)
@@ -82,7 +86,7 @@ static int	check_every_str(char **arr, int *i_s, size_t *i_e, t_argums *args)
 		if (pars_valid_up_down((arr[*i_s] + i_e), args->ud_arr))
 		{
 			printf("2\n");
-			return (error_destroy(args, 'n')); //don't forget free()
+			return (error_destroy(args, 'n'));
 		}
 	}
 	else if (arr[*i_s][*i_e] == MAP_SQ_EMPTY || arr[*i_s][*i_e] == MAP_SQ_WALL
@@ -94,28 +98,6 @@ static int	check_every_str(char **arr, int *i_s, size_t *i_e, t_argums *args)
 		{
 			return (1);
 		}
-		// int	map_fl;
-
-		// map_fl = map_pars_valid(arr + i_s);
-		// if(map_fl < 0)
-		// 	i_s = map_fl;
-		// else
-		// 	i_s += map_fl;
-		// i_e = 0;
-		// if (i_s < 0)
-		// {
-		// 	printf("3\n");
-		// 	return (error_destroy(args, 'n'));//don't forget free()
-		// }
-		// while (arr[i_s] && arr[i_s][i_e] == '\0')
-		// {
-		// 	i_s++;
-		// }
-		// if (arr[i_s] || all_agrums_got(ud_arr, txtr_arr))
-		// {
-		// 	printf("4\n");
-		// 	return (error_destroy(args, 'y'));//don't forget free()
-		// }
 		///tmp///
 		pars_destroy_up_down(args->ud_arr);
 		pars_destroy_textures(args->txtr_arr);
@@ -124,14 +106,14 @@ static int	check_every_str(char **arr, int *i_s, size_t *i_e, t_argums *args)
 	else
 	{
 		printf("5\n");
-		return (error_destroy(argums, 'y'));//don't forget free()
+		return (error_destroy(argums, 'y'));
 	}
 	return (0);
 }
 
 int	pars_arg_definition(char **arr)
 {
-	int		i_s;
+	int			i_s;
 	size_t		i_e;
 	t_up_down	*ud_arr;
 	t_textures	*txtr_arr;
@@ -150,67 +132,9 @@ int	pars_arg_definition(char **arr)
 	while (arr[i_s])
 	{
 		while (arr[i_s][i_e] == SKIP_SPACE)
-		{
 			i_e++;
-		}
-		if (arr[i_s][i_e] == '\0')
-		{
-			i_e = 0;
-		}
 		if (check_every_str(arr, &i_s, &i_e, argums))
 			return (1);
-		// else if (arr[i_s][i_e] == NORTH_SIDE_ARG || arr[i_s][i_e] == SOUTH_SIDE_ARG
-		// 	|| arr[i_s][i_e] == WEST_SIDE_ARG || arr[i_s][i_e] == EAST_SIDE_ARG)
-		// {
-		// 	//"take new texture elem" function: give me arr[i_s + i_e]
-		// 	if (pars_valid_txtr((arr[i_s] + i_e), txtr_arr))
-		// 	{
-		// 		printf("1\n");
-		// 		return (error_destroy('n')); //don't forget free()
-		// 	}
-		// }
-		// else if (arr[i_s][i_e] == FLOOR_TYPE || arr[i_s][i_e] == CEILING_TYPE)
-		// {
-		// 	//"take new up_back elem" function: give me arr[i_s + i_e]
-		// 	if (pars_valid_up_down((arr[i_s] + i_e), ud_arr))
-		// 	{
-		// 		printf("2\n");
-		// 		return (error_destroy('n')); //don't forget free()
-		// 	}
-		// }
-		// else if (arr[i_s][i_e] == MAP_SQ_EMPTY || arr[i_s][i_e] == MAP_SQ_WALL
-		// 			|| arr[i_s][i_e] == MAP_OR_NORTH || arr[i_s][i_e] == MAP_OR_SOUTH
-		// 			|| arr[i_s][i_e] == MAP_OR_EAST || arr[i_s][i_e] == MAP_OR_WEST)
-		// {
-		// 	//"for map" function: give me (arr + i_s)
-		// 	int	map_fl;
-
-		// 	map_fl = map_pars_valid(arr + i_s);
-		// 	if(map_fl < 0)
-		// 		i_s = map_fl;
-		// 	else
-		// 		i_s += map_fl;
-		// 	i_e = 0;
-		// 	if (i_s < 0)
-		// 	{
-		// 		printf("3\n");
-		// 		return (error_destroy('n'));//don't forget free()
-		// 	}
-		// 	while (arr[i_s] && arr[i_s][i_e] == '\0')
-		// 	{
-		// 		i_s++;
-		// 	}
-		// 	if (arr[i_s] || all_agrums_got(ud_arr, txtr_arr))
-		// 	{
-		// 		printf("4\n");
-		// 		return (error_destroy('y'));//don't forget free()
-		// 	}
-		// 	///tmp///
-		// 	pars_destroy_up_down(ud_arr);
-		// 	pars_destroy_textures(txtr_arr);
-		// 	return (0);
-		// }
-
 		i_e = 0;
 		i_s++;
 	}
